a4c11c85c49a6a75a2bb24df329471acb7c598a6
Will <will@pattonwebz.com>
2015-04-04 21:57:10 +0100

    fixing some errors from merge

--- mvts.php
+++ mvts.php
@@ -102,7 +102,6 @@ add_action( 'admin_init', 'register_mvts_settings' );
 	
 if ( !function_exists('register_mvts_settings') ) {
 	function register_mvts_settings() {
-<<<<<<< HEAD
 		register_setting( 'mvtsBasic-group', 'mvtsBasic', 'mvtsBasic_validate' );
 		add_settings_section('mvtsBasic', 'Basic Settings', 'basic_section_text', 'mvtsBasic-group');
 		add_settings_field('mvtsOnOff', 'Turn On or Off the tests', 'mvtsCheckboxOnOff','mvtsBasic-group', 'mvtsBasic');
@@ -129,7 +128,7 @@ function basic_section_text() {
 }
 
 
-
+ 
 
 function mvtsBasic_target_string() {
 	$options = get_option('mvtsBasic');
@@ -146,23 +145,6 @@ function mvtsBasic_validate($input) {
 	$newinput['basic_section1'] = '';
 	}*/
 	return $newinput;
-=======
-		// example syntax: register_setting( 'my_options_group', 'my_option_name', 'intval' ); 
-		register_setting( 'mvtsBasic-group', 'mvtsBasic1', 'mvtsBasic1_validate' );
-		register_setting( 'mvtsBasic-group', 'mvtsBasic2', 'mvtsBasic2_validate' );
-		add_settings_section('mvtsBasic', 'Basic Settings', 'basic_section_text', 'mvtsBasic-group');
-		add_settings_field('mvtsBaisc1_id', 'MVTS Basic Text Input', 'mvtsBasic_setting_string', 'mvts', 'mvtsBasic');
-	}
-} // end !function_exists('register_mvts_settings')
-
-function basic_section_text() {
-echo '<p>Main description of this section here.</p>';
-}
-
-function mvtsBasic_setting_string() {
-$options = get_option('plugin_options');
-echo "<input id='plugin_text_string' name='plugin_options[text_string]' size='40' type='text' value='{$options['text_string']}' />";
->>>>>>> f351420695d9c3af6b676f98877a12f87b334014
 }
 
 ?>
