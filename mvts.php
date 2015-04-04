930501f959b2a45b434fa9aba1297b0d8e3ef746
williampatton <will@pattonwebz.com>
2015-04-04 15:25:21 +0100

    Add an admin page for the plugin
    page added to top level menu, contains 2 tabs
    has code to output 2 forms for basic and advanced settings
    settings forms are ready to be used with register settings functions

--- mvts.php
+++ mvts.php
@@ -13,16 +13,87 @@ License: GPL2
  * Enqueue Javascript
  * ============================================================= */
 
-add_action( 'wp_enqueue_scripts', 'load_cohorts_scripts' );
+// register and enqueue the dependancy scripts - cohorts.js
 
-if ( !function_exists( 'load_cohorts_script' ) ) {
-    function load_cohorts_scripts() {
+add_action( 'wp_enqueue_scripts', 'load_mvts_scripts' );
+
+if ( !function_exists( 'load_mvts_scripts' ) ) {
+    function load_mvts_scripts() {
         if ( !is_admin() ) {
 			wp_register_script( 'cohorts', get_bloginfo('wpurl') . '/wp-content/plugins/mvts/js/cohorts.js', array(), 1.0, true );
             wp_enqueue_script( 'cohorts' );
 
         }
     }
-}
+} // end !function_exists( 'load_mvts_scripts' )
+
+
+// create an admin page for settings
+
+add_action( 'admin_menu', 'register_mvts_menu_page' );
+
+if ( !function_exists( 'register_mvts_menu_page' ) ) {
+
+	function register_mvts_menu_page(){
+		add_menu_page( 'MVTS', 'MVTS', 'manage_options', 'mvts', 'mvts_menu_page', 'dashicons-welcome-widgets-menus', 6 );
+	}
+
+} // end !function_exists( 'register_mvts_menu_page' )
+
+if ( !function_exists( 'mvts_menu_page' ) ) {
+	function mvts_menu_page(){ ?>
+
+		<div class="wrap">
+			<h2>MVTS Plugin</h2>
+			<div class="nav-tab-wrapper">
+				<a href="#basic-settings" class="nav-tab" data-target="#basic-settings">Basic Settings</a>
+				<a href="#advanced-settings" class="nav-tab" data-target="#advanced-settings">Certification</a>
+			</div>
+			<div class="tab-content">
+				<div id="basic-settings" class="tab-panel active">
+					<form method="post" action="options.php"> 
+						<?php 
+						settings_fields( 'mvtsBasic-group' );
+						do_settings_sections( 'mvtsBasic-group' );
+						submit_button(); 
+						?>
+					</form>
+				</div>
+				<div id="advanced-settings" class="tab-panel">
+					<form method="post" action="options.php"> 
+						<?php 
+						settings_fields( 'mvtsAdvanced-group' );
+						do_settings_sections( 'mvtsAdvanced-group' );
+						submit_button(); 
+						?>
+					</form>
+				</div>
+			</div>
+		</div>
+
+		<!-- Inline required styles and scripts on the admin page -->
+		<style type="text/css">
+			.tab-content {
+				/*max-width:650px;*/
+				margin:0 auto;
+			}
+			.tab-content .tab-panel {
+				display:none;
+			}
+			.tab-content .tab-panel.active {
+				display:block;
+			}
+		</style>
+		<script type="text/javascript">
+			jQuery( ".nav-tab-wrapper .nav-tab" ).click(function( event ) {
+				event.preventDefault();
+	  			var target = jQuery(this).attr("data-target");
+	  			jQuery( ".tab-content .active" ).removeClass('active');
+	  			jQuery(target).addClass('active');
+	  			console.log(target);
+			});
+		</script>
+	<?php }
+} // end !function_exists( 'mvts_menu_page' )
 
 ?>
