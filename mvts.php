e6747a5c5f1293df1faacd97292bcb66539cf948
williampatton <will@pattonwebz.com>
2015-04-04 21:48:01 +0100

    Added some basic settings options
    Text box for target, checkbox for on/off - with callbacks and an empty validation function
    Also fixed a typo with the options tab manes

--- mvts.php
+++ mvts.php
@@ -47,7 +47,7 @@ if ( !function_exists( 'mvts_menu_page' ) ) {
 			<h2>MVTS Plugin</h2>
 			<div class="nav-tab-wrapper">
 				<a href="#basic-settings" class="nav-tab" data-target="#basic-settings">Basic Settings</a>
-				<a href="#advanced-settings" class="nav-tab" data-target="#advanced-settings">Certification</a>
+				<a href="#advanced-settings" class="nav-tab" data-target="#advanced-settings">Advanced Settings</a>
 			</div>
 			<div class="tab-content">
 				<div id="basic-settings" class="tab-panel active">
@@ -102,21 +102,49 @@ add_action( 'admin_init', 'register_mvts_settings' );
 	
 if ( !function_exists('register_mvts_settings') ) {
 	function register_mvts_settings() {
-		// example syntax: register_setting( 'my_options_group', 'my_option_name', 'intval' ); 
-		register_setting( 'mvtsBasic-group', 'mvtsBasic1', 'mvtsBasic1_validate' );
-		register_setting( 'mvtsBasic-group', 'mvtsBasic2', 'mvtsBasic2_validate' );
+		register_setting( 'mvtsBasic-group', 'mvtsBasic', 'mvtsBasic_validate' );
 		add_settings_section('mvtsBasic', 'Basic Settings', 'basic_section_text', 'mvtsBasic-group');
-		add_settings_field('mvtsBaisc1_id', 'MVTS Basic Text Input', 'mvtsBasic_setting_string', 'mvts', 'mvtsBasic');
+		add_settings_field('mvtsOnOff', 'Turn On or Off the tests', 'mvtsCheckboxOnOff','mvtsBasic-group', 'mvtsBasic');
+		add_settings_field('target', 'Element to target', 'mvtsBasic_target_string', 'mvtsBasic-group', 'mvtsBasic');
+		
 	}
 } // end !function_exists('register_mvts_settings')
 
-function basic_section_text() {
-echo '<p>Main description of this section here.</p>';
+function mvtsCheckboxOnOff () {
+$options = get_option('mvtsBasic');
+/*if( $options['mvtsOnOff'] == '1' ) { 
+    echo '<p>The checkbox has been checked.</p>';
+} else {
+    echo '<p>The checkbox has not been checked.</p>';
+} // end if */
+$html = '<input type="checkbox" id="mvtsOnOff" name="mvtsBasic[mvtsOnOff]" value="1"' . checked( 1, $options['mvtsOnOff'], false ) . '/>';
+$html .= '<label for="mvtsOnOff">Checked = On</label>';
+
+echo $html;
 }
 
-function mvtsBasic_setting_string() {
-$options = get_option('plugin_options');
-echo "<input id='plugin_text_string' name='plugin_options[text_string]' size='40' type='text' value='{$options['text_string']}' />";
+function basic_section_text() {
+	echo '<p>Main description of this section here.</p>';
+}
+
+
+
+
+function mvtsBasic_target_string() {
+	$options = get_option('mvtsBasic');
+	// commented out code for debug
+	// print_r($options);
+	echo "<input id='target' name='mvtsBasic[target]' size='40' type='text' value='{$options['target']}' />";
+}
+
+
+function mvtsBasic_validate($input) {
+	$newinput['mvtsOnOff'] = $input['mvtsOnOff'];
+	$newinput['target'] = $input['target'];
+	/*if(!preg_match('/^[a-z0-9]{32}$/i', $newinput['basic_section1'])) {
+	$newinput['basic_section1'] = '';
+	}*/
+	return $newinput;
 }
 
 ?>
