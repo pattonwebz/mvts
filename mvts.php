48935467709c3b67d4e2571bb20c535dbc119cd3
Will <will@pattonwebz.com>
2015-04-09 03:44:42 +0100

    Rearanged code, added comments, removed unneeded comments/debug lines.
    Reorganized functions into a more logical order.
    Removed old commented out debug lines from the files
    Added dozens of new comments inline -- more needed in some sections.

--- mvts.php
+++ mvts.php
@@ -14,7 +14,6 @@ License: GPL2
  * ============================================================= */
 
 // register and enqueue the dependancy scripts - cohorts.js, jquery library
-
 add_action( 'wp_enqueue_scripts', 'load_mvts_scripts' );
 
 if ( !function_exists( 'load_mvts_scripts' ) ) {
@@ -27,13 +26,189 @@ if ( !function_exists( 'load_mvts_scripts' ) ) {
             wp_enqueue_script( 'jquery' );
         }
     }
-} // end !function_exists( 'load_mvts_scripts' )
+}
 
+// Create settings used by the admin page
+add_action( 'admin_init', 'register_mvts_settings' );
+	
+if ( !function_exists('register_mvts_settings') ) {
+	function register_mvts_settings() {
+		// register a settings group
+		register_setting( 'mvtsBasic-group', 'mvtsBasic', 'mvtsBasic_validate' );
+		// add a section to the settings group
+		add_settings_section('mvtsBasic', 'Basic Settings', 'basic_section_text', 'mvtsBasic-group');
+		// add several fields to the section in the settings group
+		add_settings_field('mvtsOnOff', 'Turn On or Off the tests', 'mvtsCheckboxOnOff','mvtsBasic-group', 'mvtsBasic');
+		add_settings_field('mvtsTrack', 'Track via Google Analytics', 'mvtsCheckboxTrack','mvtsBasic-group', 'mvtsBasic');
+		add_settings_field('testName', 'A Name to identify the test', 'mvtsBasic_testName_string', 'mvtsBasic-group', 'mvtsBasic');
+		add_settings_field('target', 'Element to target', 'mvtsBasic_target_string', 'mvtsBasic-group', 'mvtsBasic');
+		add_settings_field('selectStyle', 'Style Element to Split Test', 'mvtsBaisc_style_select', 'mvtsBasic-group', 'mvtsBasic');
+		add_settings_field('styleAtt', 'Attribute to pass', 'mvtsBasic_style_att', 'mvtsBasic-group', 'mvtsBasic');
+	}
+} // end !function_exists('register_mvts_settings')
+
+function basic_section_text() {
+	// This is the opening message for the settings section.
+	echo '<p>This is an informational section on the page.</p>';
+}
+
+// Main On/Off toggle for plugin functions
+function mvtsCheckboxOnOff () {
+	// grab the options array
+	$options = get_option('mvtsBasic');
+	// create $html variable with markup for the checkbox
+	$html = '<input type="checkbox" id="mvtsOnOff" name="mvtsBasic[mvtsOnOff]" value="1"' . checked( 1, $options['mvtsOnOff'], false ) . '/>';
+	$html .= '<label for="mvtsOnOff">Checked = On</label>';
+	// echo a checkbox
+	echo $html;
+}
+
+// Enable or disable the GA event pushing tracking feature
+function mvtsCheckboxTrack () {
+	// grab the options array
+	$options = get_option('mvtsBasic');
+	// create $html variable with markup for the checkbox
+	$html = '<input type="checkbox" id="mvtsTrack" name="mvtsBasic[mvtsTrack]" value="1"' . checked( 1, $options['mvtsTrack'], false ) . '/>';
+	$html .= '<label for="mvtsTrack">Checked = On</label>';
+	// echo a checkbox
+	echo $html;
+}
+
+function mvtsBasic_testName_string() {
+	// grab the options array
+	$options = get_option('mvtsBasic');
+	// echo a text box
+	// this is an identifier for the test used in actions and
+	// labels pushed to the tracker
+	echo "<input id='testName' name='mvtsBasic[testName]' size='40' type='text' value='{$options['testName']}' />";
+}
+
+function mvtsBasic_target_string() {
+	// grab the options array
+	$options = get_option('mvtsBasic');
+	// echo a text box
+	// expected input will be a CSS classname, an ID or a path to an element on page
+	echo "<input id='target' name='mvtsBasic[target]' size='40' type='text' value='{$options['target']}' />";
+}
+
+function mvtsBaisc_style_select() {
+	// grab the options array
+	$options = get_option('mvtsBasic');
+	// create $html variable with markup for the select box
+	// the valies are directly corilated with css properties
+    $html = '<select id="selectStyle" name="mvtsBasic[selectStyle]">';
+        $html .= '<option value="default">Select a style option...</option>';
+        // 'color'
+        $html .= '<option value="color"' . selected( $options['selectStyle'], 'color', false) . '>Color</option>';
+        // 'background-color'
+        $html .= '<option value="background-color"' . selected( $options['selectStyle'], 'background-color', false) . '>Background Color</option>';
+    $html .= '</select>';
+    // echo a select box
+    echo $html;
+}
+
+function mvtsBasic_style_att() {
+	// grab the options array
+	$options = get_option('mvtsBasic');
+	// echo a text box
+	// expected values are the actual values that would be
+	// set for the selected CSS property in the select box
+	echo "<input id='styleAtt' name='mvtsBasic[styleAtt]' size='40' type='text' value='{$options['styleAtt']}' />";
+}
+
+function mvtsBasic_validate($input) {
+	// NOTE: THIS DOES NO INPUT VALIDATION!!! BE CAREFUL!
+	// Takes the input, sets it to a new variable and then returns it.
+	// Validation should take place on the new input before it is returned
+	// so that the unsanitized input never touches the database.
+	$newinput['mvtsOnOff'] = $input['mvtsOnOff'];
+	$newinput['mvtsTrack'] = $input['mvtsTrack'];
+	$newinput['target'] = $input['target'];
+	$newinput['testName'] = $input['testName'];
+	$newinput['selectStyle'] = $input['selectStyle'];
+	$newinput['styleAtt'] = $input['styleAtt'];
+
+	// REMEMBER THIS IS STILL NOT VALIDATED/SANITIZED BEFORE IT'S RETURNED
+	return $newinput;
+}
+
+// create an admin page for settings
+add_action( 'admin_menu', 'register_mvts_menu_page' );
+
+if ( !function_exists( 'register_mvts_menu_page' ) ) {
+	function register_mvts_menu_page(){
+		add_menu_page( 'MVTS', 'MVTS', 'manage_options', 'mvts', 'mvts_menu_page', 'dashicons-welcome-widgets-menus', 6 );
+	}
+}
+
+// this is the output function for the added menu page
+if ( !function_exists( 'mvts_menu_page' ) ) {
+	function mvts_menu_page(){ ?>
+
+		<div class="wrap">
+			<h2>MVTS Plugin</h2>
+			<div class="nav-tab-wrapper">
+				<a href="#basic-settings" class="nav-tab" data-target="#basic-settings">Basic Settings</a>
+				<a href="#advanced-settings" class="nav-tab" data-target="#advanced-settings">Advanced Settings</a>
+			</div>
+			<div class="tab-content">
+				<div id="basic-settings" class="tab-panel active">
+					<form method="post" action="options.php"> 
+						<?php 
+						// outputs the mvtsBasic-group of settings.
+						settings_fields( 'mvtsBasic-group' );
+						do_settings_sections( 'mvtsBasic-group' );
+						submit_button(); 
+						?>
+					</form>
+				</div>
+				<div id="advanced-settings" class="tab-panel">
+					<p>NOTE: For future use.</p>
+					<form method="post" action="options.php"> 
+						<?php 
+						settings_fields( 'mvtsAdvanced-group' );
+						do_settings_sections( 'mvtsAdvanced-group' );
+						//submit_button(); 
+						?>
+					</form>
+				</div>
+			</div>
+		</div>
+
+		<!-- Inline required styles and scripts on the admin page. May no longer be required. -->
+		<style type="text/css">
+			.tab-content {
+				/*max-width:650px;*/
+				margin:0 auto;
+			}
+			.tab-content .tab-panel {
+				display:none;
+			}
+			.tab-content .tab-panel.active {
+				display:block;
+			}
+		</style>
+		<!-- This script adds or removes classes on the tabs based on click/active state -->
+		<script type="text/javascript">
+			jQuery( ".nav-tab-wrapper .nav-tab" ).click(function( event ) {
+				event.preventDefault();
+	  			var target = jQuery(this).attr("data-target");
+	  			jQuery( ".tab-content .active" ).removeClass('active');
+	  			jQuery(target).addClass('active');
+	  			console.log(target);
+			});
+		</script>
+	<?php }
+} // end !function_exists( 'mvts_menu_page' )
+
+// Add an action at the wp_footer call that inlines the javascript test script
 add_action( 'wp_footer', 'inline_mvtsTest_scripts' );
+
 if ( !function_exists( 'inline_mvtsTest_scripts' ) ) {
     function inline_mvtsTest_scripts() { 
     	$options = get_option('mvtsBasic');
-    	if (!$options['mvtsOnOff'] == '0') {
+    	// if the plugin is 'ON' then output the script
+    	if ($options['mvtsOnOff'] == '1') {
 	    	?>
 			<script type="text/javascript">
 			var $ = jQuery.noConflict();
@@ -46,7 +221,7 @@ if ( !function_exists( 'inline_mvtsTest_scripts' ) ) {
 					cohorts: {
 						MVTS_default_: {
 							onChosen: function() {
-								// nothing is changed here but it's still tracked
+								// Nothing is changed here but it's still tracked
 							}
 						},
 						MVTS_variant_: {
@@ -59,16 +234,17 @@ if ( !function_exists( 'inline_mvtsTest_scripts' ) ) {
 					nameSpace: 'mvts',
 					trackEvent: function(category, action, opt_label, opt_value, int_hit, cv_slot, scope) { 	
 						var len_ga = $('script[src*="analytics.js"]').length;
-							console.log( len_ga );
 						var len_gaq = $('script[src*="ga.js"]').length;
-							console.log( len_gaq );
-						<?php if ($options['mvtsTrack'] == '1'){ ?>
+						<?php // NOTE: This is a PHP if statement
+							// if Tracking is 'On' then inline the push to GA 
+							if ($options['mvtsTrack'] == '1'){ ?>
 							if (len_gaq >= 1) {
 								// if using old analytics.js
 								_gaq.push(['_trackEvent', category, action, opt_label, opt_value, int_hit]);
 							} else if (len_ga >= 1) {
 								// if using new ga.js
-								// note: using 'ga' should work in most situations however Yoasts GA plugin when in Universal mode sets it to __gaTracker
+								// note: using 'ga' should work in most situations however Yoasts GA
+								// plugin in Universal mode sets it to __gaTracker to prevent conflits
 								__gaTracker('send', 'event', category, action, opt_label, opt_value, int_hit);						
 								//ga('send', 'event', category, action, opt_label, opt_value, int_hit);	
 							} else {
@@ -88,7 +264,7 @@ if ( !function_exists( 'inline_mvtsTest_scripts' ) ) {
 				}
 				});	
 				$('<?php echo $options[target]; ?>').click(function() {
-					<?php echo $options[testName]; ?>.event('Converted'); // Track any evens with your storage adapter
+					<?php echo $options[testName]; ?>.event('Converted'); // Track any events with your storage adapter
 				});
 
 			});
@@ -98,176 +274,6 @@ if ( !function_exists( 'inline_mvtsTest_scripts' ) ) {
 		}
 	}
 
-} // end !function_exists( 'inject_mvtsTest_scripts' )
-
-// create an admin page for settings
-
-add_action( 'admin_menu', 'register_mvts_menu_page' );
-
-if ( !function_exists( 'register_mvts_menu_page' ) ) {
-
-	function register_mvts_menu_page(){
-		add_menu_page( 'MVTS', 'MVTS', 'manage_options', 'mvts', 'mvts_menu_page', 'dashicons-welcome-widgets-menus', 6 );
-	}
-
-} // end !function_exists( 'register_mvts_menu_page' )
-
-if ( !function_exists( 'mvts_menu_page' ) ) {
-	function mvts_menu_page(){ ?>
-
-		<div class="wrap">
-			<h2>MVTS Plugin</h2>
-			<div class="nav-tab-wrapper">
-				<a href="#basic-settings" class="nav-tab" data-target="#basic-settings">Basic Settings</a>
-				<a href="#advanced-settings" class="nav-tab" data-target="#advanced-settings">Advanced Settings</a>
-			</div>
-			<div class="tab-content">
-				<div id="basic-settings" class="tab-panel active">
-					<form method="post" action="options.php"> 
-						<?php 
-						settings_fields( 'mvtsBasic-group' );
-						do_settings_sections( 'mvtsBasic-group' );
-						submit_button(); 
-						?>
-					</form>
-				</div>
-				<div id="advanced-settings" class="tab-panel">
-					<form method="post" action="options.php"> 
-						<?php 
-						settings_fields( 'mvtsAdvanced-group' );
-						do_settings_sections( 'mvtsAdvanced-group' );
-						submit_button(); 
-						?>
-					</form>
-				</div>
-			</div>
-		</div>
-
-		<!-- Inline required styles and scripts on the admin page -->
-		<style type="text/css">
-			.tab-content {
-				/*max-width:650px;*/
-				margin:0 auto;
-			}
-			.tab-content .tab-panel {
-				display:none;
-			}
-			.tab-content .tab-panel.active {
-				display:block;
-			}
-		</style>
-		<script type="text/javascript">
-			jQuery( ".nav-tab-wrapper .nav-tab" ).click(function( event ) {
-				event.preventDefault();
-	  			var target = jQuery(this).attr("data-target");
-	  			jQuery( ".tab-content .active" ).removeClass('active');
-	  			jQuery(target).addClass('active');
-	  			console.log(target);
-			});
-		</script>
-	<?php }
-} // end !function_exists( 'mvts_menu_page' )
-
-// add action to trigger settings creation used by the admin page
-
-add_action( 'admin_init', 'register_mvts_settings' );
-	
-if ( !function_exists('register_mvts_settings') ) {
-	function register_mvts_settings() {
-		register_setting( 'mvtsBasic-group', 'mvtsBasic', 'mvtsBasic_validate' );
-		add_settings_section('mvtsBasic', 'Basic Settings', 'basic_section_text', 'mvtsBasic-group');
-		add_settings_field('mvtsOnOff', 'Turn On or Off the tests', 'mvtsCheckboxOnOff','mvtsBasic-group', 'mvtsBasic');
-		add_settings_field('mvtsTrack', 'Track via Google Analytics', 'mvtsCheckboxTrack','mvtsBasic-group', 'mvtsBasic');
-		add_settings_field('testName', 'A Name to identify the test', 'mvtsBasic_testName_string', 'mvtsBasic-group', 'mvtsBasic');
-		add_settings_field('target', 'Element to target', 'mvtsBasic_target_string', 'mvtsBasic-group', 'mvtsBasic');
-		add_settings_field('selectStyle', 'Style Element to Split Test', 'mvtsBaisc_style_select', 'mvtsBasic-group', 'mvtsBasic');
-		add_settings_field('styleAtt', 'Attribute to pass', 'mvtsBasic_style_att', 'mvtsBasic-group', 'mvtsBasic');
-		
-	}
-} // end !function_exists('register_mvts_settings')
-
-function mvtsCheckboxOnOff () {
-$options = get_option('mvtsBasic');
-/*if( $options['mvtsOnOff'] == '1' ) { 
-    echo '<p>The checkbox has been checked.</p>';
-} else {
-    echo '<p>The checkbox has not been checked.</p>';
-} // end if */
-$html = '<input type="checkbox" id="mvtsOnOff" name="mvtsBasic[mvtsOnOff]" value="1"' . checked( 1, $options['mvtsOnOff'], false ) . '/>';
-$html .= '<label for="mvtsOnOff">Checked = On</label>';
-
-echo $html;
-}
-
-function mvtsCheckboxTrack () {
-$options = get_option('mvtsTrack');
-/*if( $options['mvtsOnOff'] == '1' ) { 
-    echo '<p>The checkbox has been checked.</p>';
-} else {
-    echo '<p>The checkbox has not been checked.</p>';
-} // end if */
-$html = '<input type="checkbox" id="mvtsTrack" name="mvtsBasic[mvtsTrack]" value="1"' . checked( 1, $options['mvtsTrack'], false ) . '/>';
-$html .= '<label for="mvtsTrack">Checked = On</label>';
-
-echo $html;
-}
-
-function basic_section_text() {
-	echo '<p>Main description of this section here.</p>';
-}
-
-function mvtsBasic_style_att() {
-	$options = get_option('mvtsBasic');
-	// commented out code for debug
-	// print_r($options);
-	echo "<input id='styleAtt' name='mvtsBasic[styleAtt]' size='40' type='text' value='{$options['styleAtt']}' />";
-}
-function mvtsBaisc_style_select() {
- 
-   $options = get_option('mvtsBasic');
-	// commented out code for debug
-	// print_r($options);
-     
-    $html = '<select id="selectStyle" name="mvtsBasic[selectStyle]">';
-        $html .= '<option value="default">Select a style option...</option>';
-        $html .= '<option value="color"' . selected( $options['selectStyle'], 'color', false) . '>Color</option>';
-        $html .= '<option value="background-color"' . selected( $options['selectStyle'], 'background-color', false) . '>Background Color</option>';
-    $html .= '</select>';
-     
-    echo $html;
- 
-} // end sandbox_radio_element_callback 
-
-function mvtsBasic_target_string() {
-	$options = get_option('mvtsBasic');
-	// commented out code for debug
-	// print_r($options);
-	echo "<input id='target' name='mvtsBasic[target]' size='40' type='text' value='{$options['target']}' />";
-}
-
-function mvtsBasic_testName_string() {
-	$options = get_option('mvtsBasic');
-	// commented out code for debug
-	// print_r($options);
-	echo "<input id='testName' name='mvtsBasic[testName]' size='40' type='text' value='{$options['testName']}' />";
-}
-
-
-function mvtsBasic_validate($input) {
-
-	// NOTE: THIS DOES NO VALIDATION!!!
-	// It takes the input sets it into a new variable and then returns it
-	// validation should take place on the new input before it is returned
-	$newinput['mvtsOnOff'] = $input['mvtsOnOff'];
-	$newinput['mvtsTrack'] = $input['mvtsTrack'];
-	$newinput['target'] = $input['target'];
-	$newinput['testName'] = $input['testName'];
-	$newinput['selectStyle'] = $input['selectStyle'];
-	$newinput['styleAtt'] = $input['styleAtt'];
-	/*if(!preg_match('/^[a-z0-9]{32}$/i', $newinput['basic_section1'])) {
-	$newinput['basic_section1'] = '';
-	}*/
-	return $newinput;
-}
+} // end !function_exists( 'inline_mvtsTest_scripts' )
 
 ?>
